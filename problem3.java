32100e186acc20fb5b9c931d4d3a4c8ee37b9eb7
cep235 <Courtney@61-54-41-155-wireless1x.bu.edu>
2014-09-11 13:29:33 -0400

    Problem 3
    Added problem 3 to the repository

---
+++ problem3.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class problem3{
+	
+	public static void main(String[] args) {
+		
+		int[] Nuts = {2,1,3,5,4};
+		int[] Bolts = {5,3,2,1,4};
+		
+	}
+	
+	public static int Test(int x, int y){
+		if (x == y){
+			return 0;
+		}
+		else if (x > y){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	}
+	
+	public static void sort(int[] nuts, int[] bolts){
+		for (int nut=0; nut < nuts.length; nut++){
+			for (int bolt=0; bolt < bolts.length; bolt++){
+				
+			}
+		}
+	}
+	
+}
