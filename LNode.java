076d44eba4c436b3bfa6e5142003ae2d0482b41a
amitsinha1 <amitsinha1@gmail.com>
2014-09-11 19:37:17 -0400

    Create LNode.java
    the class that defines a node in a linked list

---
+++ LNode.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lsorter;
+
+/**
+ *
+ * @author amit
+ */
+public class LNode {
+        int val;
+    LNode next;
+    LNode(int x){
+    val = x;
+    next = null;
+        }
+    }
